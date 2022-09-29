e2efa7da378a068614e196dd79d5b8f160b2bd85
Brattlof <57799495+Brattlof@users.noreply.github.com>
2022-09-29 14:08:21 +0200

    Matrix

--- Source/Matrix/Matrix4x4.cpp
+++ Source/Matrix/Matrix4x4.cpp
@@ -24,29 +24,62 @@ Matrix4x4::Matrix4x4(void)
 Matrix4x4 Matrix4x4::RotateX(float angle)
 {
 	Matrix4x4 rResult;
+
 	rResult[1][1] = (float)cos(angle);
 	rResult[1][2] = (float)-sin(angle);
 	rResult[2][1] = (float)sin(angle);
 	rResult[2][2] = (float)cos(angle);
+
 	return rResult;
 }
 
 Matrix4x4 Matrix4x4::RotateY(float angle)
 {
 	Matrix4x4 rResult;
+
 	rResult[0][0] = (float)cos(angle);
 	rResult[0][2] = (float)sin(angle);
 	rResult[2][0] = (float)-sin(angle);
 	rResult[2][2] = (float)cos(angle);
+
 	return rResult;
 }
 
 Matrix4x4 Matrix4x4::RotateZ(float angle)
 {
 	Matrix4x4 rResult;
+
 	rResult[0][0] = (float)cos(angle);
 	rResult[0][1] = (float)-sin(angle);
 	rResult[1][0] = (float)sin(angle);
 	rResult[1][1] = (float)cos(angle);
+
 	return rResult;
+}
+
+//
+//
+//
+
+std::vector<float> Matrix4x4::ToVector(const Matrix4x4& m)
+{
+	std::vector<float> rVector;
+	//
+	int rCurr = 0;
+
+	for (unsigned int i = 0; i < 4; i++)
+	{
+		for (unsigned int j = 0; j < 4; j++)
+		{
+			rVector.push_back(Matrix4x4::At(m, i, j));
+			rCurr++;
+		}
+	}
+
+	return rVector;
+}
+
+std::vector<float> Matrix4x4::ToVector()
+{
+	return ToVector(*this);
 }
--- Source/Matrix/Matrix4x4.hpp
+++ Source/Matrix/Matrix4x4.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 class Matrix4x4
 {
 public:
@@ -9,8 +11,14 @@ public:
 	static Matrix4x4 RotateY(float angle);
 	static Matrix4x4 RotateZ(float angle);
 
+	static std::vector<float> ToVector(const Matrix4x4& m);
+	std::vector<float> ToVector();
+
+	static float& At(Matrix4x4 m, unsigned int row, unsigned int col) { return m.At(row, col); }
+	float& At(unsigned int row, unsigned int col) { return m_Matrix[row][col]; }
+
 	// -- Matrix4x4[i][j] -- //
-	float* operator[](unsigned int row) { return *m_Matrix; }
+	float* operator[](unsigned int row) { return m_Matrix[row]; }
 
 private:
 	float m_Matrix[4][4];
